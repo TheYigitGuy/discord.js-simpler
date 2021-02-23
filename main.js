2174105c583efedd1876148afcf6d2341f262787
TheYigitGuy <reyyanduman66@gmail.com>
2021-02-23 16:31:46 +0300

    Minor Update

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const {Client, Collection} = require("discord.js");
 const fs = require("fs");
+const path = require("path");
+
 
 
 class SimplifiedBot extends Client {
@@ -15,11 +17,11 @@ class SimplifiedBot extends Client {
      * 
      * @param {string}   path The Path to load all the commands. 
      */
-    handleCommands(path) {
+    handleCommands(pat) {
         try {
-        const cmdFiles = fs.readdirSync(path).filter(file => file.endsWith(".js" || ".ts"))
+        const cmdFiles = fs.readdirSync(pat).filter(file => file.endsWith(".js" || ".ts"))
         cmdFiles.map((file) => {
-            const command = require(`${path}/${file}`);
+            const command = require(path.join(__dirname, pat));
             this.commands.set(command.name, command)
             
                 command.aliases.map((alias) => {
@@ -33,11 +35,11 @@ class SimplifiedBot extends Client {
     /**
      * @param {string} path The Path to Load all the Events
      */
-    handleEvents(path) {
-        const eFiles = fs.readdir(path, (err, files) => {
+    handleEvents(pat) {
+        const eFiles = fs.readdir(pat, (err, files) => {
             if(err) return console.error(err);
             files.map((file) => {
-                const eFile = require(`${path}/${file}`)
+                const eFile = require(path.join(__dirname, pat))
                 this.events.set(eFile.name, eFile)
                 this.on(eFile.name, (...args) => {
                     eFile.run(this, ...args);
